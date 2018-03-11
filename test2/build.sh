cd219ebbc6eebe96dc2005610c26ff9d1f37c450
zpasal <pasalic.zaharije@gmail.com>
2018-03-11 17:04:44 -0400

    Initial commit

---
+++ test2/build.sh
@@ -0,0 +1,7 @@
+arm-none-eabi-as -o main.o main.s 
+arm-none-eabi-ld -T linker.lds -o main.elf main.o
+arm-none-eabi-nm -n main.elf
+arm-none-eabi-objcopy -O binary main.elf main.bin
+dd if=/dev/zero of=flash.bin bs=4096 count=4096
+dd if=main.bin of=flash.bin bs=4096 conv=notrunc
+qemu-system-arm -M connex -pflash flash.bin -nographic -serial /dev/null
---
+++ test3/build.sh
@@ -0,0 +1,6 @@
+arm-none-eabi-gcc -nostdlib -o main.elf -T linker.lds main.c startup.s
+arm-none-eabi-nm -n main.elf
+arm-none-eabi-objcopy -O binary main.elf main.bin
+dd if=/dev/zero of=flash.bin bs=4096 count=4096
+dd if=main.bin of=flash.bin bs=4096 conv=notrunc
+qemu-system-arm -M connex -pflash flash.bin -nographic -serial /dev/null
