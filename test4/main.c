6a9965d3dc6eeff017aeedb32ea75bae6de317c1
zaharije <zaharije@klika.ba>
2018-03-12 20:48:44 +0100

    Better FLASH & RAM configuration

--- test4/main.c
+++ test4/main.c
@@ -2,8 +2,19 @@ static int arr[] = { 1, 10, 4, 5, 6, 7 };
 static int sum;
 static const int n = sizeof(arr) / sizeof(arr[0]);
 
+volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
+
+void print_uart0(const char *s) {
+	while(*s != '\0') { /* Loop until end of string */
+ 		*UART0DR = (unsigned int)(*s); /* Transmit char */
+ 		s++; /* Next char */
+ 	}
+}
+
+
 int main() {
     for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
+    print_uart0("Hello");
 }
